167a45063389b87c5655730ab3bd4e9e3ad2ac73
iblind <iblinderman@iblinderman-macbookpro.roam.corp.google.com>
2018-08-15 15:33:08 -0400

    list of celebs update

--- src/js/graphic-live.js
+++ src/js/graphic-live.js
@@ -1,62 +1,89 @@
+import {
+	setTimeout
+} from "timers";
+
 let cleanedData;
 let nestedData;
-let last30DaysData;
-let dateToday;
+let currentDay;
+
+const $section = d3.select('#live')
+const $rankList = $section.select('ul.live__ranking')
+
+function parseDate(date) {
+	const dates = date.split('-')
+		.map(d => +d)
+	return new Date(dates[0], dates[1] - 1, dates[2])
+}
 
 function cleanData(data) {
 	return data.map(person => ({
-		// ...person,
 		article: person.article,
 		rank_people: +person.rank_people,
 		views: +person.views,
 		dateString: person.date,
-		date: new Date(person.date)
+		date: parseDate(person.date)
 	}))
 }
 
-function getTodaysDate() {
-	const currentDate = new Date();
-	return currentDate;
-}
-
-function getLast30Days(date) {
-	dateToday = getTodaysDate();
-	const thirtyDaysInMilliseconds = 31 * 24 * 60 * 60 * 1000;
-
-	return dateToday - new Date(date.key) < thirtyDaysInMilliseconds
-}
-
-
-
 
 function loadData() {
-	const dataURL = 'https://pudding.cool/2018/08/wiki-billboard-data/web/2018-top--appearance.csv'
-	d3.loadData(dataURL, (error, response) => {
+	return new Promise((resolve, reject) => {
+		const dataURL = 'https://pudding.cool/2018/08/wiki-billboard-data/web/2018-top--appearance.csv'
 
-		cleanedData = cleanData(response[0])
+		d3.loadData(dataURL, (error, response) => {
+			if (error) reject(error);
+			else {
+				cleanedData = cleanData(response[0])
 
-		nestedData = d3.nest()
-			.key(d => d.dateString)
-			.entries(cleanedData)
+				nestedData = d3.nest()
+					.key(d => d.dateString)
+					.entries(cleanedData)
+					.map(d => ({
+						...d,
+						date: parseDate(d.key)
+					}))
+				currentDay = nestedData.length - 31;
 
-		nestedData.forEach(d => {
-			d.key = new Date(d.key)
+				resolve()
+			}
 		})
-
-		last30DaysData = nestedData.filter(getLast30Days)
-		console.log(last30DaysData)
-
-
-
-
 	})
 }
 
 
+function createDOMElements() {}
+
+function updateChart() {
+	const data = nestedData[currentDay]
+
+	const $li = $rankList
+		.selectAll('li.person')
+		.data(data.values, d => d.article)
+
+	// First step: enter new items
+
+	const $enteredLi = $li.enter()
+		.append('li.person')
+		.text(d => d.article)
 
 
+	// Second step: exit old items
 
-function createDOMElements() {
+	$li.exit()
+		.remove()
+
+
+	// Third step: update pre-existing items
+
+
+	// Fourth step: update all current items
+
+	const $mergedLi = $enteredLi
+		.merge($li)
+
+	$mergedLi
+		.transition()
+		.st('top', d => d.rank_people * 20)
 
 
 }
@@ -64,8 +91,16 @@ function createDOMElements() {
 function resize() {}
 
 function init() {
+
 	loadData()
-	createDOMElements()
+		.then(() => {
+			updateChart();
+			setInterval(() => {
+				currentDay += 1
+				updateChart()
+			}, 1000)
+		})
+		.catch(console.log)
 }
 
 export default {
