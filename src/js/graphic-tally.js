4e9c35d4abac1e02aada9496a480d693a7b45b23
russellgoldenberg <russellgoldenberg@gmail.com>
2018-08-31 08:18:43 -0400

    use appearance not views

--- src/js/graphic-tally.js
+++ src/js/graphic-tally.js
@@ -1,21 +1,21 @@
-let cleanedData = []
-let nestedData = []
+let cleanedData = [];
+let nestedData = [];
 
-const $section = d3.select('#tally')
-const $tallyEstablished = $section.select('.tally__established')
-const $figure = $tallyEstablished.select('figure')
-const $svg = $figure.select('svg')
-const $gAxes = $svg.select('.g-axes')
-const $gViz = $svg.select('.g-viz')
-let $celebPaths = null;
+const COL = 'appearance_sum';
+const $section = d3.select('#tally');
+const $tallyEstablished = $section.select('.tally__established');
+const $figure = $tallyEstablished.select('figure');
+const $svg = $figure.select('svg');
+const $gAxes = $svg.select('.g-axes');
+const $gViz = $svg.select('.g-viz');
+const $celebPaths = null;
 let $voronoiGroup = null;
 let $personEnter = null;
 let $personText = null;
 
-
 const scaleX = d3.scaleTime();
 const scaleY = d3.scaleLinear();
-const voronoi = d3.voronoi()
+const voronoi = d3.voronoi();
 
 let width = 0;
 let height = 0;
@@ -26,7 +26,6 @@ const MARGIN = {
 	left: 50,
 	right: 200
 };
-
 
 function parseDate(date) {
 	const dates = date.split('-').map(d => +d);
@@ -41,44 +40,41 @@ function cleanTheData(data) {
 		views: +person.views,
 		// score: +person.score,
 		dateString: person.date,
-		// score_sum: +person.score_sum,
+		score_sum: +person.score_sum,
 		views_sum: +person.views_sum,
+		appearance_sum: +person.appearance_sum,
 		date: parseDate(person.date)
 	}));
 }
 
 function loadData() {
 	return new Promise((resolve, reject) => {
-		const timeStamped = Date.now()
-		const dataURL = `https://pudding.cool/2018/08/wiki-billboard-data/web/2018-tally-views--alive.csv?version=${timeStamped}`
-		// const dataURL = `https://pudding.cool/2018/08/wiki-billboard-data/web/2018-tally-appearance--alive.csv?version=${timeStamped}`
+		const timeStamped = Date.now();
+		// const dataURL = `https://pudding.cool/2018/08/wiki-billboard-data/web/2018-tally-views--alive.csv?version=${timeStamped}`;
+		const dataURL = `https://pudding.cool/2018/08/wiki-billboard-data/web/2018-tally-appearance--alive.csv?version=${timeStamped}`;
 
 		d3.loadData(dataURL, (error, response) => {
-
-			if (error) reject(error)
+			if (error) reject(error);
 			else {
-				cleanedData = cleanTheData(response[0])
-
+				cleanedData = cleanTheData(response[0]);
 				// console.log(response[0])
 
 				const tempNestedData = d3
 					.nest()
 					.key(d => d.name)
-					.entries(cleanedData)
+					.entries(cleanedData);
 
-				tempNestedData
-					.sort((a, b) => {
-						const maxA = a.values[a.values.length - 1].views_sum
-						const maxB = b.values[b.values.length - 1].views_sum
-						return d3.descending(maxA, maxB)
-					})
+				tempNestedData.sort((a, b) => {
+					const maxA = a.values[a.values.length - 1][COL];
+					const maxB = b.values[b.values.length - 1][COL];
+					return d3.descending(maxA, maxB);
+				});
 
-				nestedData = tempNestedData
-					.slice(0, 20)
-				resolve()
+				nestedData = tempNestedData.slice(0, 20);
+				resolve();
 			}
-		})
-	})
+		});
+	});
 }
 
 // function getMaxSum(data) {
@@ -89,42 +85,28 @@ function loadData() {
 function handleVoronoiEnter(d) {
 	const celebrityName = d.data.name;
 
-	$gViz
-		.select(`[data-name='${celebrityName}'] path`)
-		.st('stroke', '#f33')
+	$gViz.select(`[data-name='${celebrityName}'] path`).st('stroke', '#f33');
 
-	$gViz
-		.select(`[data-name='${celebrityName}'] text`)
-		.st('font-weight', '900')
+	$gViz.select(`[data-name='${celebrityName}'] text`).st('font-weight', '900');
 }
 
 function handleVoronoiLeave(d) {
-	$gViz
-		.selectAll('path')
-		.st('stroke', '#000000')
+	$gViz.selectAll('path').st('stroke', '#000000');
 
-	$gViz
-		.selectAll('text')
-		.st('font-weight', '400')
+	$gViz.selectAll('text').st('font-weight', '400');
 }
 
 function setupVoronoi() {
 	voronoi
 		.x(d => scaleX(d.date))
-		.y(d => scaleY(d.views_sum))
-		.extent([
-			[0, 0],
-			[width, height]
-		])
+		.y(d => scaleY(d[COL]))
+		.extent([[0, 0], [width, height]]);
 
-	$voronoiGroup = $gViz
-		.append('g')
-		.at('class', 'g-voronoi')
+	$voronoiGroup = $gViz.append('g').at('class', 'g-voronoi');
 
-	const mergedData = d3.merge(nestedData.map(d => d.values))
+	const mergedData = d3.merge(nestedData.map(d => d.values));
 
-	const $voronoiPath = $voronoiGroup
-		.selectAll('path')
+	const $voronoiPath = $voronoiGroup.selectAll('path');
 
 	$voronoiPath
 		.data(voronoi.polygons(mergedData))
@@ -133,107 +115,90 @@ function setupVoronoi() {
 		.on('mouseenter', handleVoronoiEnter)
 		.on('mouseout', handleVoronoiLeave)
 		// .merge($voronoiPath)
-		.attr("d", d => (d ? "M" + d.join("L") + "Z" : null))
-
+		.attr('d', d => (d ? `M${d.join('L')}Z` : null));
 }
 
 function setupChart() {
 	// bind data to dom elements
-	const $person = $gViz
-		.selectAll('g.person')
-		.data(nestedData)
+	const $person = $gViz.selectAll('g.person').data(nestedData);
 
 	// create elements
 	$personEnter = $person
 		.enter()
 		.append('g.person')
-		.at('data-name', d => d.key)
+		.at('data-name', d => d.key);
 
-	$personEnter
-		.append('path')
+	$personEnter.append('path');
 
-	$personText = $personEnter
-		.append('text')
-		.text(d => d.key)
-
+	$personText = $personEnter.append('text').text(d => d.key);
 
 	// setup scales
-	scaleX
-		.domain(d3.extent(cleanedData, d => d.date))
+	scaleX.domain(d3.extent(cleanedData, d => d.date));
 
-	scaleY
-		.domain([0, d3.max(cleanedData, d => d.views_sum)])
+	scaleY.domain([0, d3.max(cleanedData, d => d[COL])]);
+
+	console.log(scaleY.domain());
 }
 
-
-
 function render() {
-
-	const line = d3.line()
+	const line = d3
+		.line()
 		.x(d => scaleX(d.date))
-		.y(d => scaleY(d.views_sum))
-		.curve(d3.curveStepBefore)
+		.y(d => scaleY(d[COL]))
+		.curve(d3.curveStepBefore);
 
 	$personText
 		.at('y', d => {
-			const totalViews = +d.values[d.values.length - 1]['views_sum'];
-			console.log('name ' + d.values[d.values.length - 1]['article'])
-			console.log('total views ' + d.values[d.values.length - 1]['views_sum'])
-			console.log('coordinates ' + scaleY(totalViews))
-			return scaleY(totalViews)
+			const totalViews = +d.values[d.values.length - 1][COL];
+			// console.log(`name ${d.values[d.values.length - 1].article}`);
+			// console.log(`total views ${d.values[d.values.length - 1][COL]}`);
+			// console.log(`coordinates ${scaleY(totalViews)}`);
+			return scaleY(totalViews);
 		})
 		.at('x', d => {
-			const finalDate = d.values[d.values.length - 1]['date'];
-			return scaleX(finalDate)
-		})
+			const finalDate = d.values[d.values.length - 1].date;
+			return scaleX(finalDate);
+		});
 
 	// console.log(nestedData)
 
 	$gViz
 		.selectAll('.person path')
 		// .at('data-name', d => d.key)
-		.datum(d => {
-			console.log(d)
-			return d.values
-		})
-		.at('d', line)
-
+		.datum(
+			d =>
+				// console.log(d);
+				d.values
+		)
+		.at('d', line);
 }
 
 function resize() {
 	// Grab width
-	width = $figure
-		.node()
-		.offsetWidth
+	width = $figure.node().offsetWidth;
 
-	height = window
-		.innerHeight
+	height = window.innerHeight;
 
 	// update range
-	scaleX
-		.range([MARGIN.left, (width - MARGIN.right)])
+	scaleX.range([MARGIN.left, width - MARGIN.right]);
 
-	scaleY
-		.range([(height - MARGIN.bottom), MARGIN.top])
+	scaleY.range([height - MARGIN.bottom, MARGIN.top]);
 
-	$svg
-		.at('width', width)
-		.at('height', height)
+	$svg.at('width', width).at('height', height);
 
-	render()
-	setupVoronoi()
+	render();
+	setupVoronoi();
 }
 
 function init(dataPeople) {
 	// console.log(dataPeople)
 	loadData()
 		.then(() => {
-			setupChart()
-			resize()
+			setupChart();
+			resize();
 		})
-		.catch(console.log)
+		.catch(console.log);
 }
-
 
 export default {
 	init,
